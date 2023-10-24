3f52508d97ba05a574f96b9dd7a6598dc378711d
Nadia Escobar <escobarnad@gmail.com>
2023-10-24 16:05:38 +0200

    Default photo for user profile

--- models/User.model.js
+++ models/User.model.js
@@ -15,7 +15,7 @@ const userSchema = new Schema(
       type: String,
       required: [true, 'Password is required.']
     },
-    image: { type: String, default: false},
+    image: { type: String, default: "https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"},
     favoriteBooks: [String],
     favoriteAuthors: [String]
   },
