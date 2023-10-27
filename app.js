0af7d3b5dc98c60001334e10c114e80502642388
Nadia Escobar <escobarnad@gmail.com>
2023-10-27 17:11:20 +0200

    netlify to CORS

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require("./config")(app);
 
 app.use(
     cors({
-      origin: [`http://localhost:5173`,`http://127.0.0.1:5173`],
+      origin: [`http://localhost:5173`,`http://127.0.0.1:5173`, `https://booklovers-ironhack.netlify.app`],
         })
       );
 
