54b8b27318509f657f8526075f39374374cb5c1f
Rin-o <rino091370@gmail.com>
2023-10-31 11:01:21 +0100

    delete and update routes done

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -23,8 +23,8 @@ router.get('/:bookId/reviews', isAuthenticated, async (req, res) => {
 router.post('/', isAuthenticated, async (req, res) => {
   try {
     const newReview = await Review.create({...req.body, user: req.payload.userId})
-
-    res.status(201).json({ review: newReview})
+    const fullNewReview = await Review.findById(newReview._id).populate('user')
+    res.status(201).json({ review: fullNewReview })
   } catch (error) {
     console.log(error)
     res.status(400).json({ error: 'Failed to create a review' });
@@ -34,18 +34,19 @@ router.post('/', isAuthenticated, async (req, res) => {
 // Update a review by ID
 router.put('/:id', isAuthenticated, async (req, res) => {
   
-    try {
-      const newReview = await Review.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      res.status(202).json({ review: newReview })
-    } catch (error) {
-      console.log(error)
-      res.status(400).json({ error: 'Failed to update the review' })
-    }
+  try {
+    const oneReview = await Review.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    const fullOneReview = await Review.findById(oneReview._id).populate('user')
+    res.status(202).json({ review: fullOneReview })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ error: 'Failed to update the review' })
+  }
 });
 
 // Delete a review by ID
 
-router.delete('/:id', isAuthenticated, async (req, res) => {
+router.delete('/:bookId/reviews/:id', isAuthenticated, async (req, res) => {
     await Review.findByIdAndDelete(req.params.id)
     res.status(202).json({ message: 'Review deleted' })
   })
